9f9a0e67c31b67ca37cce3051e1a3d87e79fbcaa
Natasha Kuksa <natasha.kuksa.96@gmail.com>
2023-03-30 22:07:58 +0200

    task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', handlerSubmitForm);
+
+function handlerSubmitForm(event) {
+  event.preventDefault();
+
+  const delay = Number(event.currentTarget.delay.value);
+  const step = Number(event.currentTarget.step.value);
+  const amount = Number(event.currentTarget.amount.value);
+
+  let amountDelay = delay;
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, amountDelay).then(onResolve).catch(onReject);
+    amountDelay += step;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(position, delay);
+      } else {
+        reject(position, delay);
+      }
+    }, delay);
+  });
+}
+
+function onResolve(position, delay) {
+  Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+
+function onReject(position, delay) {
+  Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 }
